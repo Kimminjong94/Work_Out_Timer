a4b613052994b4342172493fc476ce3db9389e2f
minjongkim <rlaalswhd200@naver.com>
2022-01-27 19:09:06 +0900

    added setting files and more views

---
+++ Work_Out_Timer/Record/RecordVC.swift
@@ -0,0 +1,16 @@
+//
+//  RecordVC.swift
+//  Work_Out_Timer
+//
+//  Created by 김민종 on 2022/01/27.
+//
+
+import UIKit
+
+class RecordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Work_Out_Timer/Setting/SettingVC.swift
@@ -0,0 +1,16 @@
+//
+//  SettingVC.swift
+//  Work_Out_Timer
+//
+//  Created by 김민종 on 2022/01/27.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
