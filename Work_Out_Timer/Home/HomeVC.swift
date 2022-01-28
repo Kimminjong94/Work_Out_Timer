a231b7076ce4f9303d1cb1fb18ce63567ddb89e4
minjongkim <rlaalswhd200@naver.com>
2022-01-28 16:51:52 +0900

    added timer function

--- Work_Out_Timer/Home/HomeVC.swift
+++ Work_Out_Timer/Home/HomeVC.swift
@@ -8,9 +8,54 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var startStopLabel: UILabel!
+    
+    
+    var myTimer = Timer()
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        
+        if count == 0 {
+            self.startStopLabel.text = "Start"
+        } else {
+            self.myTimer.invalidate()
+            self.startStopLabel.text = "Stop"
+        }
+        DispatchQueue.main.async() {
+            self.myTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateCounter), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func count30Tapped(_ sender: UIButton) {
+        self.count += 30
+        self.countLabel.text = "\(self.count)"
+
+    }
+    @IBAction func count60Tapped(_ sender: UIButton) {
+        self.count += 60
+        self.countLabel.text = "\(self.count)"
+
+    }
+    @IBAction func count120Tapped(_ sender: UIButton) {
+        self.count += 120
+        self.countLabel.text = "\(self.count)"
+
+    }
+    
+    @objc func updateCounter() {
+        if count > 0 {
+            print("\(count) seconds to the end of the world")
+            count -= 1
+            self.countLabel.text = "\(self.count)"
+        }
+
     }
 }
 
