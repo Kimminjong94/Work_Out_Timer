e3d9e751745280642dbc6605b1d3f489ae273f18
minjongkim <rlaalswhd200@naver.com>
2022-02-02 15:39:26 +0900

    finished timer logic! working fine

--- Work_Out_Timer/Home/HomeVC.swift
+++ Work_Out_Timer/Home/HomeVC.swift
@@ -15,29 +15,39 @@ class HomeVC: UIViewController {
     
     var myTimer = Timer()
     var count = 0
+    var isTimerRunning = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.startButton.layer.cornerRadius = 12
         
+        // 레이블 입력 시 버튼 색변환
+        self.startButton.isEnabled = false
+//        self.countLabel.addTarget(self, action: #selector(self.textDidChange(_:)), for: .valueChanged)
     }
     @IBAction func startButtonTapped(_ sender: UIButton) {
             
-        
-        myTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateCounter), userInfo: nil, repeats: true)
-        
-        if count == 0 {
-            self.startStopLabel.text = "Start"
-        } else {
+        if isTimerRunning == false {
+            runTimer()
             self.startStopLabel.text = "Stop"
+        } else {
+            isTimerRunning = false
+            self.myTimer.invalidate()
+            self.startStopLabel.text = "Start"
         }
 
+    }
+    
+    func runTimer() {
+        myTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        isTimerRunning = true
     }
     
     @IBAction func count30Tapped(_ sender: UIButton) {
         self.count += 5
         self.countLabel.text = "\(self.count)"
-
+        
+        
     }
     @IBAction func count60Tapped(_ sender: UIButton) {
         self.count += 60
@@ -55,9 +65,14 @@ class HomeVC: UIViewController {
         if count > 0 {
             count -= 1
             print("\(count)")
+        } else if count < 1 {
+            self.startStopLabel.text = "Start"
+            self.myTimer.invalidate()
         }
-
-
+    }
+    @objc func textDidChange(_ sender: Any?) {
+        self.startButton.isEnabled = true
+        self.startButton.backgroundColor = UIColor.black
     }
 }
 
