514e5197b4ea984a2e7b58d2b319ca79d7bb9e68
minjongkim <rlaalswhd200@naver.com>
2022-02-04 16:36:12 +0900

    added CLTypingLabel Library

---
+++ Work_Out_Timer/LaunchVC.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchVC.swift
+//  Work_Out_Timer
+//
+//  Created by 김민종 on 2022/02/04.
+//
+
+import UIKit
+
+
+class LaunchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
