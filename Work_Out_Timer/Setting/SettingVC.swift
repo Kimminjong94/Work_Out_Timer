c568b5cc5145736a4d1550b11bdda226d38714b8
minjongkim <rlaalswhd200@naver.com>
2022-02-09 18:44:06 +0900

    added logout button and function

--- Work_Out_Timer/Setting/SettingVC.swift
+++ Work_Out_Timer/Setting/SettingVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingVC: UIViewController {
 
@@ -20,5 +21,19 @@ class SettingVC: UIViewController {
         super.viewWillDisappear(animated)
         self.navigationController?.navigationBar.isHidden = false
     }
-
+    @IBAction func logoutPressed(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            //팝루트뷰컨트롤러 가능하면 사용하기 왜냐하면 스택을 다 지우고 처음ㅇ화면으로 돌아가기 때문
+//            navigationController?.popViewController(animated: true)
+            
+            print("Error signing out: %@", signOutError)
+        }
+        
+        guard let navigation = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "BaseTabBarViewController") as? BaseTabBarViewController else { return }
+        navigation.modalPresentationStyle = .overFullScreen
+        self.present(navigation, animated: true, completion: nil)
+    }
+    
 }
