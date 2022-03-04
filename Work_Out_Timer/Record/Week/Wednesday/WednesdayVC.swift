3bdd54f914fcd2c5e00cd219a405f1b11269b482
minjongkim <rlaalswhd200@naver.com>
2022-03-04 18:05:57 +0900

    added wednesday collection view

--- Work_Out_Timer/Record/Week/Wednesday/WednesdayVC.swift
+++ Work_Out_Timer/Record/Week/Wednesday/WednesdayVC.swift
@@ -13,8 +13,40 @@ class WednesdayVC: UIViewController {
         super.viewDidLoad()
 
     }
-    
-
-
 
 }
+
+extension WednesdayVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return currentData.count
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == self.tuesdayCV {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TuesdayCell", for: indexPath) as? TuesdayCell else {return UICollectionViewCell()}
+//            cell.currentIdx = indexPath.row
+//            cell.delegate = self
+            cell.tuesdayName.text = currentData[indexPath.row].body ?? ""
+            cell.delegate = self
+            return cell
+        }
+            return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if collectionView == self.tuesdayCV {
+            return CGSize(width: self.tuesdayCV.bounds.width, height: 80)
+        } else {
+            return CGSize(width: 100, height: 100)
+        }
+    }
+}
+
